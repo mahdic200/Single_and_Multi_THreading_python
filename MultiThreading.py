dc5f4b11ff202fa536a57d3c8986b7cd2179ebdd
mahdic200 <rmahdi170@gmail.com>
2023-04-07 13:23:58 +0430

    initializing project

---
+++ MultiThreading.py
@@ -0,0 +1,137 @@
+import requests, os, timeit
+from threading import Thread
+
+
+class MultiThreading:
+    ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+    
+    def __init__(self) -> None:
+        self.__threads = []
+        self.__urls = []
+        self.__startTime = 0
+        self.__endTime = 0
+        self.__logs = []
+        self.__downloadedImagesFolder = "MultiThreadingImages"
+        if not os.path.exists(MultiThreading.standardPath(self.__downloadedImagesFolder)):
+            os.mkdir(MultiThreading.standardPath(self.__downloadedImagesFolder))
+        
+    @property
+    def totalTime(self):
+        return self.__endTime - self.__startTime
+
+    @property
+    def urls(self):
+        return self.__urls
+    
+    @property
+    def logs(self):
+        return self.__logs
+
+    def __downloadImage(self, url):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            if response.status_code != 204:
+                path = MultiThreading.standardPath(self.__downloadedImagesFolder, self.__getImageName(url))
+                with open(path, "wb+") as f:
+                    f.write(response.content)
+        except Exception as error:
+            self.__logs.append({"message":error, "url":url})
+
+    def __getImageName(self, url):
+        return url.split("/")[-1]
+
+    def __initUrls(self):
+        path = MultiThreading.standardPath("urls.txt")
+        with open(path, 'r') as f:
+            allUrls = f.readlines()
+        for url in allUrls:
+            self.__urls.append(url.replace("\n", ""))
+
+    def __main(self):
+        self.__initUrls()
+        i = 0
+        for url in self.__urls:
+            temp = Thread(target=self.__downloadImage, args=[url])
+            self.__threads.append(temp)
+            self.__threads[i].start()
+            self.__threads[i].join()
+            i += 1
+
+    def fire(self):
+        self.__startTime = timeit.default_timer()
+        self.__main()
+        self.__endTime = timeit.default_timer()
+        for i in self.__logs:
+            path = MultiThreading.standardPath("error_logs.txt")
+            with open(path, 'a+') as f:
+                f.write(i["url"] + "\n")
+                f.write(str(i["message"]) + "\n\n")
+
+    def standardPath(*args):
+        return os.path.join(MultiThreading.ABS_PATH, *args)
+
+sth = MultiThreading()
+sth.fire()
+
+print(f"total process time : {sth.totalTime} seconds .")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SingleThreading.py
@@ -0,0 +1,125 @@
+import requests, os, timeit
+
+
+class SingleThreading:
+    ABS_PATH = os.path.dirname(os.path.abspath(__file__))
+    
+    def __init__(self) -> None:
+        self.__urls = []
+        self.__startTime = 0
+        self.__endTime = 0
+        self.__logs = []
+        self.__downloadedImagesFolder = "SingleThreadingImages"
+        if not os.path.exists(SingleThreading.standardPath(self.__downloadedImagesFolder)):
+            os.mkdir(SingleThreading.standardPath(self.__downloadedImagesFolder))
+        
+    @property
+    def totalTime(self):
+        return self.__endTime - self.__startTime
+
+    @property
+    def urls(self):
+        return self.__urls
+    
+    @property
+    def logs(self):
+        return self.__logs
+
+    def __downloadImage(self, url):
+        # try:
+            response = requests.get(url)
+            response.raise_for_status()
+            if response.status_code != 204:
+                path = SingleThreading.standardPath(self.__downloadedImagesFolder, self.__getImageName(url))
+                with open(path, "wb+") as f:
+                    f.write(response.content)
+        # except Exception as error:
+        #     self.__logs.append({"message":error, "url":url})
+
+    def __getImageName(self, url):
+        return url.split("/")[-1]
+
+    def __initUrls(self):
+        path = SingleThreading.standardPath("urls.txt")
+        with open(path, 'r') as f:
+            allUrls = f.readlines()
+        for url in allUrls:
+            self.__urls.append(url.replace("\n", ""))
+
+    def __main(self):
+        self.__initUrls()
+        for url in self.__urls:
+            self.__downloadImage(url)
+
+    def fire(self):
+        self.__startTime = timeit.default_timer()
+        self.__main()
+        self.__endTime = timeit.default_timer()
+
+    def standardPath(*args):
+        return os.path.join(SingleThreading.ABS_PATH, *args)
+
+# sth = SingleThreading()
+# sth.fire()
+
+# print(f"total process time : {sth.totalTime} seconds .")
+
+print(SingleThreading.ABS_PATH)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
